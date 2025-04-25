984ff508a73de3cb1b9376c7e4d6671d15cfbaca
Sakar Bhandari <bhandarisakar@outlook.com>
2025-04-25 14:07:27 +0545

    DSA DAY 1

---
+++ 1)array.py
@@ -0,0 +1,32 @@
+# ❓ Problem Statement:
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# 📌 Problem Link: Two Sum – LeetCode #1
+# input = [4, 4, 5, 3]
+# target = 8
+
+# output = []
+
+# for i in range(len(input)):
+#     for j in range(i+1, len(input)):
+#         if input[i]+input[j] == target:
+#             if input[i]!= input[j]:
+#                 output.append(input[i])
+#                 output.append(input[j])
+#                 break   
+        
+# print(output)
+
+#Optimized Code using HASHMAP
+nums = [3, 2, 1, 0,9,10,4,5,6,7,8]
+target = 8
+num_map = {}  # value -> index
+
+for i, num in enumerate(nums):
+    complement = target - num
+    print(complement)
+    if complement in num_map:
+        print([num_map[complement], i])
+        break
+    num_map[num] = i
---
+++ 2)array.py
@@ -0,0 +1,12 @@
+# 🧮 Problem 2: Best Time to Buy and Sell Stock (Easy)
+# 📌 LeetCode #121
+# 🔗 Best Time to Buy and Sell Stock – LeetCode
+
+# ❓ Problem Statement:
+# You are given an array prices where prices[i] is the price of a stock on the i-th day.
+
+# You want to buy one stock and sell one stock, but must buy before selling.
+
+# Return the maximum profit you can achieve. If you cannot achieve any profit, return 0.
+
+prices = [7, 1, 5, 3, 6, 4]
